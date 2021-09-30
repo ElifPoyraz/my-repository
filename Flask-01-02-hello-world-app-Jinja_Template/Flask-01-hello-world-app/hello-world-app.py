eaf83bd360bdec386f21403ddf08c418c18590f9
Elif Poyraz <elif.pehlivan@outlook.com>
2021-09-29 20:36:25 -0500

    My first app with Flask

---
+++ Flask-01-02-hello-world-app-Jinja_Template/Flask-01-hello-world-app/hello-world-app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def head():
+    return "Hello World!"
+
+
+@app.route("/second")
+def second():
+    return "This is the second page"
+
+@app.route('/third/subthird')
+def third():
+    return "this is the subpage of third page"
+
+@app.route("/forth/<string:id>")
+def forth(id):
+    return f"Id of this page is {id}"
+
+
+if __name__=="__main__":
+    app.run(debug=True)
